35e8c675e14341aa163ef0ce30c5f2731c5e6ae1
Max Tan <yenseow@gmail.com>
2022-06-26 23:06:03 +0000

    Blackjack v2

--- blackjack.rb
+++ blackjack.rb
@@ -1,9 +1,11 @@
-# Methods that format text
+LINE = "-------------------------------------"
+
+# Methods that affect text output
 def prompt(msg)
   puts "==> #{msg}"
 end
 
-def display_cards(cards, d1=', ', d2='and')
+def display_all_cards(cards, d1=', ', d2='and')
   # Lists the cards nicely for the player.
   output = ''
 
@@ -25,14 +27,26 @@ def conceal_last_card(card_string)
 end
 
 def display_dealer_cards(cards)
-  conceal_last_card(display_cards((cards)))
+  conceal_last_card(display_all_cards((cards)))
 end
 
-def goodbye_msg
-  prompt("Thank you for playing Blackjack!")
+def display_bj_winner(outcome)
+  case outcome
+  when 'player' then prompt("You have a blackjack hand. You won!")
+  when 'tie'    then prompt("You and the dealer have blackjack hands. It's a tie!")
+  when 'dealer' then prompt("The dealer has a blackjack hand. You lost. Better luck next time!")
+  end
 end
 
-# Methods that create or modify cards
+def display_winner(outcome)
+  case outcome
+  when 'player' then prompt("You won!")
+  when 'tie'    then prompt("It's a tie!")
+  when 'dealer' then prompt("You lost. Better luck next time!")
+  end
+end
+
+# Methods that create, read, or modify cards
 def initialize_deck
   suit = %w(Ace 2 3 4 5 6 7 8 9 10 Jack Queen King)
   deck = suit * 4
@@ -47,38 +61,26 @@ def hit!(cards, deck)
   cards << deck.shift
 end
 
-# Methods that read cards
 def get_total_value(cards)
-  # Split the aces from the non-aces.
-  aces, non_aces = cards.partition { |card| card == 'Ace' }
+  total_value = 0
 
-  # Calculate the value of the non-aces first.
-  non_aces_value = get_non_aces_value(non_aces)
-
-  # Calculate the value of the aces based on the previous value.
-  # Combine both values for the total value.
-  non_aces_value + get_aces_value(aces, non_aces_value)
-end
-
-def get_non_aces_value(cards)
-  cards.reduce(0) do |value, card|
-    %w(2 3 4 5 6 7 8 9 10).include?(card) ? value += card.to_i : value += 10
-  end
-end
-
-def get_aces_value(cards, value)
-  cards.reduce(0) do |aces_value, card|
-    if value + 11 <= 21
-      value += 11
-      aces_value += 11
+  cards.each do |card|
+    if %w(2 3 4 5 6 7 8 9 10).include?(card)
+      total_value += card.to_i
+    elsif %w(Jack Queen King).include?(card)
+      total_value += 10
     else
-      value += 1
-      aces_value += 1
+      total_value += 11
     end
   end
+
+  aces = cards.count("Ace")
+  aces.times { total_value -= 10 if total_value > 21 } if aces > 0
+
+  total_value
 end
 
-# Methods concerning endgame conditions.
+# Methods that involve endgame situations.
 def blackjack?(cards)
   cards.include?('Ace') &&
     (cards.include?('10') || cards.include?('Jack') ||
@@ -87,6 +89,18 @@ end
 
 def busted?(cards)
   get_total_value(cards) > 21
+end
+
+def detect_bj_winner(player, dealer)
+  if blackjack?(dealer) && blackjack?(player)
+    'tie'
+  elsif blackjack?(dealer)
+    'dealer'
+  elsif blackjack?(player)
+    'player'
+  else
+    nil
+  end
 end
 
 def detect_winner(player, dealer)
@@ -99,95 +113,90 @@ def detect_winner(player, dealer)
   end
 end
 
-def display_winner(outcome)
-  case outcome
-  when 'player' then prompt("You win!")
-  when 'tie'    then prompt("It's a tie!")
-  when 'dealer' then prompt("You lose.")
-  end
+def play_again
+  puts LINE
+  prompt("Would you like to play another round of Blackjack? (y/n)")
+  answer = gets.chomp.downcase
+  answer[0] == 'y'
 end
 
-# Start of game. Deal cards.
-prompt("Welcome to Blackjack!")
-
-deck = initialize_deck
-
-dealer_cards = deal_cards!(deck)
-prompt("Dealer's cards: #{display_dealer_cards(dealer_cards)}.")
-
-player_cards = deal_cards!(deck)
-prompt("Your cards: #{display_cards(player_cards)}.")
-
-# Check for blackjacks. Consider refactoring this code.
-if blackjack?(dealer_cards) && blackjack?(player_cards)
-  prompt("Both of you have blackjacks. It's a tie!")
-  goodbye_msg
-  exit
-elsif blackjack?(dealer_cards)
-  prompt("Dealer has blackjack. You lose.")
-  goodbye_msg
-  exit
-elsif blackjack?(player_cards)
-  prompt("You have blackjack. You win!")
-  goodbye_msg
-  exit
-end
-
-# Player's turn
+# Game loops until the player decides to quit.
 loop do
-  prompt("Would you like to hit ('h') or stay ('s')?")
+  # Start of game. Deal cards.
+  puts LINE
+  prompt("Welcome to Blackjack! ♠ ♥ ♦ ♣")
+  deck = initialize_deck
+  dealer_cards = deal_cards!(deck)
+  player_cards = deal_cards!(deck)
 
-  action = gets.chomp
-
-  if action.downcase[0] == 'h'
-    hit!(player_cards, deck)
-    prompt("Your cards: #{display_cards(player_cards)}.")
-    break if busted?(player_cards)
-    next
+  # Check for blackjack hands at the outset.
+  bj_winner = detect_bj_winner(player_cards, dealer_cards)
+  if bj_winner != nil
+    prompt("Dealer's cards: #{display_all_cards(dealer_cards)}.")
+    prompt("Your cards: #{display_all_cards(player_cards)}.")
+    display_bj_winner(bj_winner)
+    play_again ? next : break
   end
 
-  break if action.downcase[0] == 's'
+  prompt("Dealer's cards: #{display_dealer_cards(dealer_cards)}.")
+  prompt("Your cards: #{display_all_cards(player_cards)}.")
 
-  prompt("That was an invalid choice. Please enter 'h' or 's'.")
+  # Player's turn
+  loop do
+    prompt("Your points: #{get_total_value(player_cards)}.")
+    prompt("Would you like to hit (h) or stay (s)?")
+
+    action = gets.chomp.downcase
+    if action[0] == 'h'
+      hit!(player_cards, deck)
+      prompt("You chose to hit.")
+      prompt("Your cards are now: #{display_all_cards(player_cards)}.")
+      break if busted?(player_cards)
+      next
+    end
+
+    break if action[0] == 's'
+
+    prompt("That was an invalid choice. Please enter 'h' or 's'.")
+  end
+
+  # There are two ways of reaching this point of the code.
+  # 1) The player busted. 2) The player did not bust and chose to stay.
+  if busted?(player_cards)
+    prompt("You lost. You have busted 21.")
+    prompt("The total value of your cards is #{get_total_value(player_cards)}.")
+    prompt("Better luck next time!")
+    play_again ? next : break
+  else
+    prompt("You chose to stay.")
+  end
+
+  # Dealer's turn
+  puts LINE
+  prompt("It is the dealer's turn now.")
+
+  loop do
+    break if get_total_value(dealer_cards) >= 17
+    hit!(dealer_cards, deck)
+  end
+
+  prompt("Dealer's cards: #{display_all_cards(dealer_cards)}.")
+  prompt("Dealer's points: #{get_total_value(dealer_cards)}.")
+
+  # Check if the dealer busted 21.
+  if busted?(dealer_cards)
+    prompt("The dealer has busted 21.")
+    prompt("You won!")
+    play_again ? next : break
+  end
+
+  prompt("Your cards: #{display_all_cards(player_cards)}.")
+  prompt("Your points: #{get_total_value(player_cards)}.")
+
+  winner = detect_winner(player_cards, dealer_cards)
+  display_winner(winner)
+  play_again ? next : break
 end
 
-# Check if the player busted 21.
-if busted?(player_cards)
-  prompt("You lost. You have busted 21.")
-  prompt("The total value of your cards is #{get_total_value(player_cards)}.")
-  goodbye_msg
-  exit
-else
-  prompt("You chose to stay.")
-end
-
-# Dealer's turn
-prompt("It is now the dealer's turn.")
-
-loop do
-  break if get_total_value(dealer_cards) >= 17
-  hit!(dealer_cards, deck)
-end
-
-prompt("Dealer's cards: #{display_cards(dealer_cards)}.")
-prompt("Dealer's points: #{get_total_value(dealer_cards)}.")
-
-# Check if the dealer busted 21.
-if busted?(dealer_cards)
-  prompt("The dealer has busted 21.")
-  prompt("You win!")
-  goodbye_msg
-  exit
-end
-
-prompt("Your cards: #{display_cards(player_cards)}.")
-prompt("Your points: #{get_total_value(player_cards)}.")
-
-winner = detect_winner(player_cards, dealer_cards)
-display_winner(winner)
-goodbye_msg
-
-# Consider implementing a replay mechanic.
-# prompt("Do you want to play another round of Blackjack? (y/n)")
-# answer = gets.chomp
-# break unless answer.downcase[0] == 'y'
+puts LINE
+prompt("Thank you for playing Blackjack!")
